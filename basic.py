97f68249af6921b2e2c4700345db1323ea42cb8c
Creative Lab <creativelab@Creatives-MacBook-Air.local>
2023-05-06 16:50:28 -0500

    Initial commit

---
+++ basic.py
@@ -0,0 +1,60 @@
+# Basic syntax of Python
+
+# Example to print text
+
+# print("Hi, Pycharm")
+
+
+# Example of Storing Variables
+
+# Variables are connected to a value
+
+# message = "Hello welcome to Python World"
+# print(message)
+
+# Going to combine messages.
+
+# message = "I hope you are doing well"
+# print(message)
+
+# We received the value of the two messages on two separate lines
+
+# A string is a series of characters.
+# Example 'This is a string' also "This is a string"
+# The example below using title () changes each word to a capital word.
+
+# name = "mario.fletcher"
+# print(name.title())
+
+# Another example to change the word to an uppercase and lowercase
+
+# name = "mario.fletcher"
+# print(name.upper())
+# print(name.lower())
+
+
+# Using Variables in Strings
+# f is used to format
+
+# first_name = "mario"
+# last_name = "fletcher"
+# full_name = f"{first_name} {last_name}"
+# print(full_name)
+
+# Using numbers in Python
+
+# Floats
+# print(3.5)
+
+# Integer
+# print(3)
+
+# Example using a variable plus a function.
+
+# calculation_to_units = 24
+# name_of_units = "hours"
+# print(f"20 days are {20 * calculation_to_units} {name_of_units}")
+# print(f"35 days are {20 * calculation_to_units} {name_of_units}")
+# print(f"50 days are {20 * calculation_to_units} {name_of_units}")
+# print(f"110 days are {20 * calculation_to_units} {name_of_units}")
+
